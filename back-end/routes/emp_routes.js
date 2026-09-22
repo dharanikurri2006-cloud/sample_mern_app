27224601d247a8ccc2ce2e8b16207ba1ddbdce9e
Dharani Kurri <dharanikurri2006@gmail.com>
2026-09-22 10:42:33 +0530

    second commit

--- back-end/routes/emp_routes.js
+++ back-end/routes/emp_routes.js
@@ -1,16 +1,18 @@
 let express=require('express');
 let router=express.Router();
 let {users}=require('../models/users');
-
-router.get("/viewtasks",(req,res)=>{
-    res.send("view tasks page called");
- })
+let bcrypt=require('bcrypt');
 
  router.post("/register",async (req,res)=>{
    let data=req.body;
+   data.password=await bcrypt.hash(data.password,10);
    let newuser=new users(data);
    let result=await newuser.save();
    res.send(result);
+ })
+
+router.get("/viewtasks",(req,res)=>{
+    res.send("view tasks page called");
  })
 
   router.post("/login",(req,res)=>{
